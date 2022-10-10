233abe8b347cc3f6e2d1b822cd6b122cd365e70e
ArturDerbeniov <74465913+ArturDerbeniov@users.noreply.github.com>
2022-10-10 22:39:36 +0300

    Add files via upload

--- js/site.js
+++ js/site.js
@@ -2,6 +2,62 @@ document.addEventListener("click", eventDocClick, false);
 
 gsap.registerPlugin(ScrollTrigger);
 
+
+let triggerNameStr1 = ".headerPage-mainPage__animImgs.animationType1";
+gsap.to(triggerNameStr1, {
+		duration:0.7, 
+		scaleX:0.95, scaleY:0.95, rotationX:0, rotationY:0, rotationZ:0, x:-250, y:150, z:0, skewX:0, skewY:0,
+		scrollTrigger: {
+			markers: true,
+			trigger: triggerNameStr1,
+			toggleActions: "play none none reverse",
+			toggleClass: "active",
+			start: "top 70px",
+			end: "bottom top",
+			id: "anim1",
+		}
+	}
+);
+
+let triggerNameStr2 = ".headerPage-mainPage__animImgs.animationType2",
+	animImgsHeader = document.querySelectorAll(triggerNameStr2 + " img");
+gsap.to(triggerNameStr2, {
+		duration:0.7, 
+		scaleX:0.95, scaleY:0.95, rotationX:0, rotationY:0, rotationZ:0, x:-250, y:150, z:0, skewX:0, skewY:0,
+		scrollTrigger: {
+			markers: true,
+			trigger: triggerNameStr2,
+			toggleActions: "play none none reverse",
+			toggleClass: "active",
+			start: "top 70px",
+			end: "bottom top",
+			id: "anim2",
+			onEnter: () => {
+				gsap.to(animImgsHeader, {scale:1.15, stagger: 0.06, duration:0.6, ease:"circ.out"}, "<+=.6");
+			},
+			onLeaveBack: () => {
+				gsap.to(animImgsHeader, {scale:1, stagger: 0.06, duration:0.6, ease:"circ.out"}, "<+=.6");
+			}
+		}
+	}
+);
+/*let tl = gsap.timeline({
+	// duration:1.5, ease: "power2.out",
+	// scaleX:0.95, scaleY:0.95, rotationX:0, rotationY:0, rotationZ:0, x:-250, y:150, z:0, skewX:0, skewY:0,
+	scrollTrigger: {
+		markers: true,
+		trigger: ".headerPage-mainPage__animImgs",
+		toggleActions: "play none reverse none",
+		start: "top 100px",
+		end: "bottom top",
+		toggleClass: "active"
+	}
+});*/
+/*tl.to(".headerPage-mainPage__animImgs", {
+	duration:1.5, ease: "power2.out",
+	scaleX:0.95, scaleY:0.95, rotationX:0, rotationY:0, rotationZ:0, x:-250, y:150, z:0, skewX:0, skewY:0,
+});*/
+// tl.fromTo(".headerPage-mainPage__animImgs img", {rotation:+10}, {rotation:-10}, "<+=.5");
 //=============================================//
 ///////////////// pined pics ////////////////////
 //=============================================//
